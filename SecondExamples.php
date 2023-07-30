7c8bb1161afe8e200a2092f2d029ddd6696d678c
skavinda18 <136300196+skavinda18@users.noreply.github.com>
2023-07-30 23:23:09 +0530

    Add files via upload

---
+++ SecondExamples.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<?php   // tag eka patan ganna widiya
+
+$a=8;
+$b=5;
+	$c=$a+$b;
+	$d=$a-$b;
+	$e=$a*$b;
+	$f=$a/$b;
+	echo("<h1>" .($a+$b). "</h1>");
+	echo("<h1>The Dif Is $d</h1>");
+	echo("<h1>The MUltiplication Is $e</h1>");
+	echo("<h1>The Division Is $f</h1>");
+
+       //Tag eka iwara karana widiya
+?>
+</body> 
+</html>
